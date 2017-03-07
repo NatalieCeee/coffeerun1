13d51b742af9f536b57cb3505154584a78e44aa2
NatalieCeee <natlovesu4@gmail.com>
2017-03-06 21:46:29 -0800

    Add files via upload

--- scripts/tests.js
+++ scripts/tests.js
@@ -1,78 +1,55 @@
-//Figure 8.10
-QUnit.test('DataStore', function(assert) {
-    var ds = new App.DataStore();
+QUnit.test( "Assignment 5 Testing - DataStore", function( assert ) {
+  var ds = new App.DataStore();
+  ds.add('m@bond.com', 'tea');
+  ds.add('james@bond.com', 'eshpressho');
+  assert.deepEqual(ds.getAll(), {'m@bond.com':"tea", 'james@bond.com': "eshpressho"});
 
-
-    ds.add('m@bond.com', 'tea');
-    ds.add('james@bond.com', 'eshpressho');
-
-    assert.deepEqual(ds.getAll(), {
-        'james@bond.com': 'eshpressho',
-        'm@bond.com': 'tea'
-    });
-
-    ds.remove('james@bond.com');
-
-    assert.deepEqual(ds.getAll(), {
-        'm@bond.com': 'tea'
-    });
-
-    assert.equal(ds.get('m@bond.com'), 'tea');
-
-    assert.equal(ds.get('james@bond.com'), undefined);
-
+  ds.remove('james@bond.com');
+  assert.deepEqual(ds.getAll(), {'m@bond.com': 'tea'} )
+  assert.equal(ds.get('m@bond.com'), 'tea');
+  assert.equal(ds.get('james@bond.com'), undefined);
 });
 
+QUnit.test( "Truck Stuff", function( assert ) {
+  var db = new App.DataStore();
+  var myTruck = new App.Truck('7', db);
 
-
-//Figure 8.32
-//Problem : no method to return values so they could be compared.
-//Created a function to get all the values
-
-QUnit.test('truck', function(assert) {
   myTruck.createOrder({
-      emailAddress: 'me@goldfinger.com',
-      coffee: 'double mocha'
+    emailAddress: 'me@goldfinder.com',
+    coffee: 'double mocha'
   });
-
-    myTruck.createOrder({
-        emailAddress: 'dr@no.com',
-        coffee: 'decaf'
-    });
-
-    myTruck.createOrder({
-        emailAddress: 'm@bond.com',
-        coffee: 'earl grey'
-    });
-
-    myTruck.printOrders();
-
-    assert.deepEqual(myTruck.getAllTruck(), {
-        'me@goldfinger.com': {
+  myTruck.createOrder({
+    emailAddress: 'dr@no.com',
+    coffee: 'decaf'
+  });
+  myTruck.createOrder({
+    emailAddress: 'm@bond.com',
+    coffee: 'earl grey'
+  });
+  // The problem here was that the Truck object had no way of retrieving all the order data
+  // Solution: write a getAllOrders function for the prototype
+  myTruck.printOrders();
+  assert.deepEqual(myTruck.getAllOrders(), {
+    'me@goldfinder.com': {
+      'coffee': 'double mocha',
+      'emailAddress': 'me@goldfinder.com'
+    },
+    'dr@no.com': {
+      'coffee': 'decaf',
+      'emailAddress': 'dr@no.com'
+    },
+    'm@bond.com': {
+      'coffee': 'earl grey',
+      'emailAddress': 'm@bond.com'
+    }
+  });
+  myTruck.deliverOrder('dr@no.com');
+  myTruck.deliverOrder('m@bond.com');
+  myTruck.printOrders();
+  assert.deepEqual(myTruck.getAllOrders(), {
+        'me@goldfinder.com': {
             'coffee': 'double mocha',
-            'emailAddress': 'me@goldfinger.com'
-        },
-        'dr@no.com': {
-            'coffee': 'decaf',
-            'emailAddress': 'dr@no.com'
-        },
-        'm@bond.com': {
-            'coffee': 'earl grey',
-            'emailAddress': 'm@bond.com'
+            'emailAddress': 'me@goldfinder.com'
         }
-    });
-
-    myTruck.deliverOrder('dr@no.com');
-
-    myTruck.deliverOrder('m@bond.com');
-
-    myTruck.printOrders();
-
-    assert.deepEqual(myTruck.getAllTruck(), {
-        'me@goldfinger.com': {
-            'coffee': 'double mocha',
-            'emailAddress': 'me@goldfinger.com'
-        }
-    });
-
+});
 });
